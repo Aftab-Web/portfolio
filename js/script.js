f1c1b1d9b984d50661ef744bca4452dfa894bca9
Aftab-Web <aftab629179806@gmail.com>
2023-11-17 16:58:04 +0530

    update

--- js/script.js
+++ js/script.js
@@ -5,27 +5,28 @@ tl.to('.away',{
     height:0,
     duration: .3,
 })
-tl.from(".content ul li",{
+gsap.from(".content ul li",{
     x: 0,
     duration: 0.6,
-    delay: 2000
+    delay: 2
 })
-tl.from(".t-ab",{
+
+gsap.to('.fill',{
+    width: '75%',
+    duration: .5,
+    scrollTrigger:{
+        trigger: '.fill',
+        scroller: 'body',
+    }
+})
+gsap.to(".t-ab",{
     scale:1.3,
     duration:1,
     yoyo: true,
     repeat:-1
 })
 
-tl.to('.fill',{
-    width: '75%',
-    duration: .5,
-    scrollTrigger:{
-        trigger: '.fill',
-        scroller: 'body',
 
-    }
-})
 // tl.from(".sidebar a p",{
 //     y:5,
 //     duration:.5,
@@ -40,6 +41,16 @@ $(function(){
     $('.close').click(function(){
         $('.sidebar').css( 'transform' , "translateX(-1000px)")
     })
+
+
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+           $('.top').addClass('newClass');
+        } else {
+           $('top').removeClass('newClass');
+        }
+    });
 })
 
 AOS.init();
